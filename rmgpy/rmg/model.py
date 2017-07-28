312601ed1ea09efe87e6ed7c5dc199323fcb86bf
alongd <alongd@mit.edu>
2017-07-27 23:12:32 -0400

    changed to max 350 for NG_HTP-Oxy

--- rmgpy/rmg/model.py
+++ rmgpy/rmg/model.py
@@ -921,7 +921,7 @@ class CoreEdgeReactionModel:
         coreSpeciesCount, coreReactionCount, edgeSpeciesCount, edgeReactionCount = self.getModelSize()
 
 
-        numSpeciesInSeed = 978 #NG_HTP
+        numSpeciesInSeed = 350 #NG_HTP-Oxy
         if coreSpeciesCount -4 > numSpeciesInSeed: # species in seed + Ar, He, Ne, N2
             logging.info('')
             logging.info('')
