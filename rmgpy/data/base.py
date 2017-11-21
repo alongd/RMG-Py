686fad4c66e8d07bbde8e2f0744fa52b622daf22
alongd <alongd@mit.edu>
2017-11-20 19:48:03 -0500

    Avoid generating iso,orphic res structures with wrong * labels
    This fixed training reactions where wrong * labels are sent back on
    isomoprphic resonance structures.

--- rmgpy/data/base.py
+++ rmgpy/data/base.py
@@ -282,7 +282,7 @@ class Database:
                 if line.strip() == '' and adjlist.strip() != '':
                     # Finish this adjacency list
                     species = Species().fromAdjacencyList(adjlist)
-                    species.generateResonanceIsomers()
+                    species.generateResonanceIsomers(keepIsomorphic=False)  # isomorphic res structures may have wrong labeling
                     label = species.label
                     if label in speciesDict:
                         raise DatabaseError('Species label "{0}" used for multiple species in {1}.'.format(label, str(self)))
@@ -294,7 +294,7 @@ class Database:
                 if adjlist.strip() != '':
                     # Finish this adjacency list
                     species = Species().fromAdjacencyList(adjlist)
-                    species.generateResonanceIsomers()
+                    species.generateResonanceIsomers(keepIsomorphic=False)
                     label = species.label
                     if label in speciesDict:
                         raise DatabaseError('Species label "{0}" used for multiple species in {1}.'.format(label, str(self)))
