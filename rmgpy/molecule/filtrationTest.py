0e0cdff7d8273801a1cfdf3cfaf8cc3d7e954075
alongd <alongd@mit.edu>
2018-01-22 09:21:35 -0500

    fixup! filtrationTest

--- rmgpy/molecule/filtrationTest.py
+++ rmgpy/molecule/filtrationTest.py
@@ -32,6 +32,7 @@ from .resonance import *
 from .filtration import *
 ################################################################################
 
+
 class FiltrationTest(unittest.TestCase):
 
     def basic_filtration_test(self):
@@ -60,69 +61,65 @@ class FiltrationTest(unittest.TestCase):
         mol3 = molecule.Molecule().fromAdjacencyList(adj3)
 
         mol_list = [mol1,mol2,mol3]
-
+        octet_deviation_list = get_octet_deviation_list(mol_list)
         filtered_list = filter_structures(mol_list)
 
+        self.assertEqual(octet_deviation_list,[1,3,3])
         self.assertEqual(len(filtered_list), 1)
         self.assertTrue(all([atom.charge == 0 for atom in filtered_list[0].vertices]))
 
+    def penalty_for_N_val_9_test(self):
+        """Test that N atoms with valance 9 get penalized in the octet deviation score"""
+        adj = """
+        multiplicity 2
+        1 N u1 p0 c0 {2,S} {3,T}
+        2 O u0 p2 c0 {1,S} {4,S}
+        3 N u0 p1 c0 {1,T}
+        4 H u0 p0 c0 {2,S}
+        """
+        mol = molecule.Molecule().fromAdjacencyList(adj)
+        octet_deviation = get_octet_deviation(mol)
+        self.assertEqual(octet_deviation, 2)
 
+    def penalty_for_O4tc_test(self):
+        """Test that an O4tc atomType with octet 8 gets penalized in the octet deviation score"""
+        adj = """
+        1 S u0 p1 c0 {2,S} {3,T}
+        2 O u0 p3 c-1 {1,S}
+        3 O u0 p1 c+1 {1,T}
+        """
+        mol = molecule.Molecule().fromAdjacencyList(adj)
+        octet_deviation = get_octet_deviation(mol)
+        self.assertEqual(octet_deviation, 1)
+        self.assertEqual(mol.vertices[2].atomType.label, 'O4tc')
 
+    def penalty_for_S_triple_S_test(self):
+        """Test that an S#S substructure in a molecule gets penalized in the octet deviation score"""
+        adj = """
+        1  C u0 p0 c0 {3,S} {5,S} {6,S} {7,S}
+        2  C u0 p0 c0 {4,S} {8,S} {9,S} {10,S}
+        3  S u0 p0 c0 {1,S} {4,T} {11,D}
+        4  S u0 p1 c0 {2,S} {3,T}
+        5  H u0 p0 c0 {1,S}
+        6  H u0 p0 c0 {1,S}
+        7  H u0 p0 c0 {1,S}
+        8  H u0 p0 c0 {2,S}
+        9  H u0 p0 c0 {2,S}
+        10 H u0 p0 c0 {2,S}
+        11 O u0 p2 c0 {3,D}
+        """
+        mol = molecule.Molecule().fromAdjacencyList(adj)
+        octet_deviation = get_octet_deviation(mol)
+        self.assertEqual(octet_deviation, 1)
 
-# tests specific species that get penalized, test OS and adjBirads, test the reactive attribute
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def penalty_for_fake_SO_test(self):
+        """Test that an SO structure wich has a multiplicity 3 and two radicals as the correct [S][O] structure, yet has
+        both radicals on the same site gets penalized in the octet deviation score"""
+        adj = """
+        multiplicity 3
+        1 O u0 p2 c0 {2,D}
+        2 S u2 p1 c0 {1,D}
+        """
+        mol = molecule.Molecule().fromAdjacencyList(adj)
+        octet_deviation = get_octet_deviation(mol)
+        self.assertEqual(octet_deviation, 3)
