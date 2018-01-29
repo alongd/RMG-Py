c43211f672696376e64efc2e33f47cc4bcbb99a0
alongd <alongd@mit.edu>
2018-01-29 12:09:23 -0500

    Added a family irreversibility test
    for Singlet_O2_to_triplet and 1,2-Birad_to_alkene

--- rmgpy/data/kinetics/familyTest.py
+++ rmgpy/data/kinetics/familyTest.py
@@ -590,7 +590,7 @@ class TestGenerateReactions(unittest.TestCase):
             path=os.path.join(settings['test_data.directory'], 'testing_database'),
             thermoLibraries=[],
             reactionLibraries=[],
-            kineticsFamilies=['H_Abstraction', 'R_Addition_MultipleBond'],
+            kineticsFamilies=['H_Abstraction', 'R_Addition_MultipleBond','Singlet_O2_to_triplet','1,2-Birad_to_alkene'],
             depository=False,
             solvation=False,
             testing=True,
@@ -673,3 +673,14 @@ multiplicity 2
                 mapping[atom] = product.molecule[0].getLabeledAtom(label)
 
             self.assertTrue(expected_products[i].isIsomorphic(product.molecule[0], mapping))
+
+    def test_irreversible_reaction(self):
+        """Test that the Singlet_O2_to_triplet and 1,2-Birad_to_alkene families generate irreversible reactions."""
+
+        reactant = [Molecule(SMILES='O=O')]
+        reactionList = self.database.kinetics.families['Singlet_O2_to_triplet'].generateReactions(reactant)
+        self.assertFalse(reactionList[0].reversible)
+
+        reactant = [Molecule(SMILES='[O][S]=O')]
+        reactionList = self.database.kinetics.families['1,2-Birad_to_alkene'].generateReactions(reactant)
+        self.assertFalse(reactionList[0].reversible)
