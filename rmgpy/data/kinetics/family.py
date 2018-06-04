51fcd90fe773c68baa97b53812ea8e90f3993a0e
alongd <alongd@mit.edu>
2018-06-04 14:38:18 -0400

    skip degeneracy error

--- rmgpy/data/kinetics/family.py
+++ rmgpy/data/kinetics/family.py
@@ -1616,9 +1616,9 @@ class KineticsFamily(Database):
                 logging.error("Reactant: {0!r}".format(reactant))
             for product in reaction.products:
                 logging.error("Product: {0!r}".format(product))
-            raise KineticsError(('Unable to calculate degeneracy for reaction {0} '
-                                 'in reaction family {1}. Expected 1 reaction '
-                                 'but generated {2}').format(reaction, self.label, len(reactions)))
+            # raise KineticsError(('Unable to calculate degeneracy for reaction {0} '
+            #                      'in reaction family {1}. Expected 1 reaction '
+            #                      'but generated {2}').format(reaction, self.label, len(reactions)))
         return reactions[0].degeneracy
         
     def __generateReactions(self, reactants, products=None, forward=True, prod_resonance=True,
