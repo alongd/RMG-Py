4e8103bddf85daf2dd5f2b9977abd6b9cbe114f5
Alon Grinberg Dana <alon@technion.ac.il>
2022-07-03 17:11:11 +0300

    d! kinetics

--- arkane/kinetics.py
+++ arkane/kinetics.py
@@ -168,6 +168,12 @@ class KineticsJob(object):
         if isinstance(tunneling, Wigner) and tunneling.frequency is None:
             tunneling.frequency = (self.reaction.transition_state.frequency.value_si, "cm^-1")
         elif isinstance(tunneling, Eckart) and tunneling.frequency is None:
+            print('Eckart tunneling')
+            print([reactant.conformer.E0.value_si
+                                      for reactant in self.reaction.reactants])
+            print(self.reaction.transition_state.conformer.E0.value_si)
+            print([product.conformer.E0.value_si
+                                      for product in self.reaction.products])
             tunneling.frequency = (self.reaction.transition_state.frequency.value_si, "cm^-1")
             tunneling.E0_reac = (sum([reactant.conformer.E0.value_si
                                       for reactant in self.reaction.reactants]) * 0.001, "kJ/mol")
